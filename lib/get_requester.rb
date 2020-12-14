9e629762f9041ff4c80e8253f0b61281973ea703
ryan.bollettino@gmail.com <bollo@bollo-laptop.localdomain>
2020-12-14 16:13:55 -0600

    commit tree 3304b956e21fb5e249bb7fb0fe49c9d25854d47b

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,27 @@
-# Write your code here
+require 'net/http'
+require 'open-uri'
+require 'json'
+
+class GetRequester  #request JSON data
+
+    #URL = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
+    def initialize(url)
+    end
+    
+    def get_response_body
+         uri = URI.parse(URL) #creates variable for parsed data from site
+        response = Net::HTTP.get_response(uri) #data after request from site
+        response.body
+    end
+
+    def parse_json
+        people = JSON.parse(self.get_response_body)
+        people.collect do |person|
+            person
+        end
+    end
+
+
+   #return Ruby Hash/Array
+end
+
